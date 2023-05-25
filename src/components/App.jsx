d7cd07609abe6e1eb2779c83137d0c33b12c94ff
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-05-25 10:38:16 +0300

    Виджет отзывов

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
+import { useState } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import styles from './App.module.css';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleLeaveFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const handleResetFeedback = () => {
+    setGood(0);
+    setNeutral(0);
+    setBad(0);
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.app}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleLeaveFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+        <button className={styles.resetButton} onClick={handleResetFeedback}>
+          Reset Statistics
+        </button>
+      </Section>
     </div>
   );
 };
+
+export default App;
